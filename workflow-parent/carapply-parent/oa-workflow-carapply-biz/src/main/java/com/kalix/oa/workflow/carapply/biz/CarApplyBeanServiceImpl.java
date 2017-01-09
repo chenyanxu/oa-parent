6298690fb2ddf8dc46aaad664f56f2badead951e
zangyanming <zyanming2002@163.com>
2017-01-09 14:02:09 +0800

    取消车辆状态机测试

--- workflow-parent/carapply-parent/oa-workflow-carapply-biz/src/main/java/com/kalix/oa/workflow/carapply/biz/CarApplyBeanServiceImpl.java
+++ workflow-parent/carapply-parent/oa-workflow-carapply-biz/src/main/java/com/kalix/oa/workflow/carapply/biz/CarApplyBeanServiceImpl.java
@@ -6,7 +6,6 @@ import com.kalix.oa.workflow.carapply.api.biz.ICarApplyBeanService;
 import com.kalix.oa.workflow.carapply.api.dao.ICarApplyBeanDao;
 import com.kalix.oa.workflow.carapply.entities.CarApplyBean;
 
-import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -27,11 +26,11 @@ public class CarApplyBeanServiceImpl extends WorkflowGenericBizServiceImpl<ICarA
     @Override
     public Map getVariantMap(Map map,CarApplyBean bean) {
         //测试状态机
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("config.xml");
-        statemachineService.initFSM(is, "START");
-        String currentState = statemachineService.getCurrentState();
-        statemachineService.processFSM("MOVELEFT");
-        currentState = statemachineService.getCurrentState();
+//        InputStream is = this.getClass().getClassLoader().getResourceAsStream("config.xml");
+//        statemachineService.initFSM(is, "START");
+//        String currentState = statemachineService.getCurrentState();
+//        statemachineService.processFSM("MOVELEFT");
+//        currentState = statemachineService.getCurrentState();
 
         map.put("city", bean.isCity());
         return map;
