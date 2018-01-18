9b8c88615f31e23966ef419a981db35e4bd8d461
yangze8818 <yangze_lmkj@163.com>
2018-01-18 08:42:22 +0800

    新增统计菜单、服务、pom引用

--- workflow-parent/redheadapply-parent/oa-workflow-redheadapply-biz/src/main/java/com/kalix/oa/workflow/redheadapply/biz/RedheadApplyBeanServiceImpl.java
+++ workflow-parent/redheadapply-parent/oa-workflow-redheadapply-biz/src/main/java/com/kalix/oa/workflow/redheadapply/biz/RedheadApplyBeanServiceImpl.java
@@ -1,6 +1,19 @@
 package com.kalix.oa.workflow.redheadapply.biz;
 
+import com.github.abel533.echarts.axis.CategoryAxis;
+import com.github.abel533.echarts.axis.ValueAxis;
+import com.github.abel533.echarts.code.Magic;
+import com.github.abel533.echarts.code.Tool;
+import com.github.abel533.echarts.feature.MagicType;
+import com.github.abel533.echarts.json.GsonOption;
+import com.github.abel533.echarts.series.Bar;
+import com.github.abel533.echarts.style.ItemStyle;
+import com.github.abel533.echarts.style.itemstyle.Normal;
+import com.kalix.framework.core.api.persistence.JpaStatistic;
+import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.framework.core.api.persistence.JsonStatus;
+import com.kalix.framework.core.api.web.model.QueryDTO;
+import com.kalix.framework.core.util.SerializeUtil;
 import com.kalix.middleware.statemachine.api.biz.IStatemachineService;
 import com.kalix.middleware.workflow.biz.WorkflowGenericBizServiceImpl;
 import com.kalix.oa.system.dict.api.biz.IOADictBeanService;
@@ -13,9 +26,9 @@ import com.kalix.oa.workflow.redheadapply.entities.DocumentBean;
 import com.kalix.oa.workflow.redheadapply.entities.DocumentConfigBean;
 import com.kalix.oa.workflow.redheadapply.entities.RedheadApplyBean;
 
+import javax.persistence.Tuple;
 import java.io.InputStream;
-import java.util.Calendar;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author sunlf
@@ -171,5 +184,116 @@ public class RedheadApplyBeanServiceImpl extends WorkflowGenericBizServiceImpl<I
 
     public void setOaDictBeanService(IOADictBeanService oaDictBeanService) {
         this.oaDictBeanService = oaDictBeanService;
+    }
+
+    public JsonData getStatisticByDocType(String jsonStr) {
+        Map<String, String> jsonMap = null;
+        Map<String, String> barmap = new HashMap<>();
+        if (jsonStr != null && !jsonStr.isEmpty()) {
+            jsonMap = SerializeUtil.json2Map(jsonStr);
+        }
+        QueryDTO dto = new QueryDTO();
+        // select doctype,count(number) from oa_workflow_redheadapply group by doctype;
+        JpaStatistic jpaStatistic = new JpaStatistic();
+        jpaStatistic.setGroupBys(new String[]{"docType"});
+        jpaStatistic.setSelectNotStatistics(new String[]{"docType"});
+        jpaStatistic.setSelectStatistics(new String[]{"number"});
+        jpaStatistic.setStatisticTypes(new JpaStatistic.Statistic[]{JpaStatistic.Statistic.COUNT});
+        Map<String, String> params = jpaStatistic.getStatisticParam();
+        if (jsonMap != null && !jsonMap.isEmpty()) {
+            for (Map.Entry<String, String> entry : jsonMap.entrySet()) {
+                params.put(entry.getKey(),entry.getValue());
+            }
+        }
+        //params.put("docType:in","5,6");
+        dto.setJsonMap(params);
+        JsonData data = dao.getAllByStatistic(dto);
+        List<Tuple> list = data.getData();
+
+        JsonData d1 = new JsonData();
+        List<Map<String, String>> dataList = new ArrayList<>();
+//        String barData = testBar(true);
+        String barData = testBar1(list);
+
+        barmap.put("option", barData);
+        dataList.add(barmap);
+        d1.setData(dataList);
+        return d1;
+    }
+
+    private String testBar1(List<Tuple> list){
+        String[] types = new String[list.size()];
+        int[] datas = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            types[i] = String.valueOf(list.get(i).get(0));
+            datas[i] = Integer.parseInt(list.get(i).get(1).toString());
+        }
+        String title = "红头文件分类统计";
+        GsonOption option = new GsonOption();
+        option.title(title); // 标题
+        // 工具栏
+        option.toolbox().show(true).feature(Tool.mark, // 辅助线
+                Tool.dataView, // 数据视图
+                new MagicType(Magic.line, Magic.bar),// 线图、柱状图切换
+                Tool.restore,// 还原
+                Tool.saveAsImage);// 保存为图片
+        option.tooltip().show(true).formatter("{a} <br/>{b} : {c}");//显示工具提示,设置提示格式
+        option.legend(title);// 图例
+        Bar bar = new Bar(title);// 图类别(柱状图)
+        CategoryAxis category = new CategoryAxis();// 轴分类
+        category.data(types);// 轴数据类别
+        // 循环数据
+        for (int i = 0; i < datas.length; i++) {
+            int data = datas[i];
+            Map<String, Object> map = new HashMap<>();
+            map.put("value", data);
+            bar.data(map);
+        }
+        option.xAxis(category);// x轴
+        option.yAxis(new ValueAxis());// y轴
+        option.series(bar);
+        return option.toString();
+    }
+
+    private String testBar(Boolean isHorizontal){
+        String[] citis = { "广州", "深圳", "珠海", "汕头", "韶关", "佛山" };
+        int[] datas = { 6030, 7800, 5200, 3444, 2666, 5708 };
+        String[] colors = { "rgb(2,111,230)", "rgb(186,73,46)", "rgb(78,154,97)", "rgb(2,111,230)", "rgb(186,73,46)", "rgb(78,154,97)" };
+        String title = "地市数据";
+        GsonOption option = new GsonOption();
+        option.title(title); // 标题
+        // 工具栏
+        option.toolbox().show(true).feature(Tool.mark, // 辅助线
+        Tool.dataView, // 数据视图
+        new MagicType(Magic.line, Magic.bar),// 线图、柱状图切换
+        Tool.restore,// 还原
+        Tool.saveAsImage);// 保存为图片
+
+        option.tooltip().show(true).formatter("{a} <br/>{b} : {c}");//显示工具提示,设置提示格式
+
+        option.legend(title);// 图例
+        Bar bar = new Bar("城市数据");// 图类别(柱状图)
+        CategoryAxis category = new CategoryAxis();// 轴分类
+        category.data(citis);// 轴数据类别
+        // 循环数据
+        for (int i = 0; i < citis.length; i++) {
+            int data = datas[i];
+            String color = colors[i];
+            // 类目对应的柱状图
+            Map<String, Object> map = new HashMap<String, Object>(2);
+            map.put("value", data);
+            map.put("itemStyle", new ItemStyle().normal(new Normal().color(color)));
+            bar.data(map);
+        }
+        if (isHorizontal) {// 横轴为类别、纵轴为值
+            option.xAxis(category);// x轴
+            option.yAxis(new ValueAxis());// y轴
+        } else {// 横轴为值、纵轴为类别
+            option.xAxis(new ValueAxis());// x轴
+            option.yAxis(category);// y轴
+        }
+
+        option.series(bar);
+        return option.toString();
     }
 }
